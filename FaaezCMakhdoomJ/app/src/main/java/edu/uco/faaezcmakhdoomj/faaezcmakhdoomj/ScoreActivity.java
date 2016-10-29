61abb7ed498fc0aabdba5e4c66ba301194e21361
Faaez <fchishti@uco.edu>
2016-10-29 15:20:50 -0500

    Activities added

---
+++ FaaezCMakhdoomJ/app/src/main/java/edu/uco/faaezcmakhdoomj/faaezcmakhdoomj/ScoreActivity.java
@@ -0,0 +1,13 @@
+package edu.uco.faaezcmakhdoomj.faaezcmakhdoomj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
--- FaaezCMakhdoomJ/app/src/main/java/edu/uco/faaezcmakhdoomj/faaezcmakhdoomj/SplashActivity.java
+++ FaaezCMakhdoomJ/app/src/main/java/edu/uco/faaezcmakhdoomj/faaezcmakhdoomj/SplashActivity.java
@@ -3,14 +3,24 @@ package edu.uco.faaezcmakhdoomj.faaezcmakhdoomj;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        mHandler.postDelayed(mUpdateTimeTask, 2000);
+
     }
+
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
